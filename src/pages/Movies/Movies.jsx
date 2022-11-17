035f2f56b4abb45798e0762c575e68d0eb7d7700
prorochenko <33101252+prorochenko@users.noreply.github.com>
2022-11-17 18:25:32 +0200

    cat and reviews btn

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,22 +1,22 @@
-import { searchMovies } from '../../components/Services/API';
-import { useEffect, useState } from 'react';
-import { NavLink } from 'react-router-dom';
+// import { searchMovies } from '../../components/Services/API';
+// import { useEffect, useState } from 'react';
+// import { NavLink } from 'react-router-dom';
 
 export const Movies = () => {
-  const [movies, setMovies] = useState('');
+  // const [movies, setMovies] = useState('');
 
-  useEffect(() => {
-    searchMovies().then(setMovies);
-  }, []);
+  // useEffect(() => {
+  //   searchMovies().then(setMovies);
+  // }, []);
 
   return (
     <>
       <input></input>
       <button>Search</button>
       <div>
-        {movies.map(movie => (
+        {/* {movies.map(movie => (
           <NavLink to={`${movie.id}`}>{movie.id}</NavLink>
-        ))}
+        ))} */}
       </div>
     </>
   );
