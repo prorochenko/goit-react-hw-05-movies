695d2052e8c35c9b629caab2c023aaa923dc55d6
prorochenko <33101252+prorochenko@users.noreply.github.com>
2022-11-17 18:24:11 +0200

    cat and reviews btn

--- src/pages/MoviesDetails/MoviesDetails.jsx
+++ src/pages/MoviesDetails/MoviesDetails.jsx
@@ -1,10 +1,12 @@
 import { getMoviesDetails } from '../../components/Services/API';
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation, useNavigate } from 'react-router-dom';
 
 export const MoviesDetails = () => {
   const [details, setDetails] = useState(null);
   const { movieID } = useParams();
+  const location = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
     getMoviesDetails(movieID).then(setDetails);
@@ -14,18 +16,43 @@ export const MoviesDetails = () => {
     <>
       {details ? (
         <div>
-          <img
-            src={`https://image.tmdb.org/t/p/w500/${details.poster_path}`}
-            alt={details.title}
-            width="300"
-            height="450"
-          />
-          <h1>{details.title}</h1>
-          <p>User Score: {details.vote_average}%</p>
-          <h2>Overview</h2>
-          <p>{details.overview}</p>
-          <h2>Genres</h2>
-          <p>{details.genres.map(genre => genre.name)}</p>
+          <div>
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${details.poster_path}`}
+              alt={details.title}
+              width="300"
+              height="450"
+            />
+            <h1>{details.title}</h1>
+            <p>User Score: {details.vote_average}%</p>
+            <h2>Overview</h2>
+            <p>{details.overview}</p>
+            <h2>Genres</h2>
+            <p>{details.genres.map(genre => genre.name)}</p>
+          </div>
+          <div>
+            <h2>Additional Information</h2>
+            <ul>
+              <button
+                type="button"
+                onClick={() => {
+                  navigate(`cast`, { state: { from: location.state?.from } });
+                }}
+              >
+                Cast
+              </button>
+              <button
+                type="button"
+                onClick={() => {
+                  navigate(`reviews`, {
+                    state: { from: location.state?.from },
+                  });
+                }}
+              >
+                Reviews
+              </button>
+            </ul>
+          </div>
         </div>
       ) : (
         ''
