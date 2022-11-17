4fa83f801d517182d28dcbdc370dc54a5ad88ee3
prorochenko <33101252+prorochenko@users.noreply.github.com>
2022-11-17 18:33:21 +0200

    movies Page

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,23 +1,42 @@
-// import { searchMovies } from '../../components/Services/API';
-// import { useEffect, useState } from 'react';
-// import { NavLink } from 'react-router-dom';
+import { searchMovies } from '../../components/Services/API';
+import { useEffect, useState } from 'react';
+import { NavLink, useSearchParams, useLocation } from 'react-router-dom';
 
 export const Movies = () => {
-  // const [movies, setMovies] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get('query');
+  const location = useLocation();
 
-  // useEffect(() => {
-  //   searchMovies().then(setMovies);
-  // }, []);
+  useEffect(() => {
+    if (!query) {
+      return;
+    }
+    searchMovies(query).then(setMovies);
+  }, [query]);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const input = e.currentTarget;
+    setSearchParams({ query: input.elements.query.value });
+    input.reset();
+  };
 
   return (
     <>
-      <input></input>
-      <button>Search</button>
-      <div>
-        {/* {movies.map(movie => (
-          <NavLink to={`${movie.id}`}>{movie.id}</NavLink>
-        ))} */}
-      </div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="query"></input>
+        <button type="submit">Search</button>
+      </form>
+      <ul>
+        {movies.map(movie => (
+          <li>
+            <NavLink to={`${movie.id}`} state={{ from: location }}>
+              {movie.title}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
