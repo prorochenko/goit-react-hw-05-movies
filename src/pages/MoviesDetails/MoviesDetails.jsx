1e84aa529851aa5d2c1eecb867334a8f566f927f
prorochenko <33101252+prorochenko@users.noreply.github.com>
2022-11-17 19:49:18 +0200

    added status

--- src/pages/MoviesDetails/MoviesDetails.jsx
+++ src/pages/MoviesDetails/MoviesDetails.jsx
@@ -9,14 +9,83 @@ export const MoviesDetails = () => {
   const { movieID } = useParams();
   const location = useLocation();
   const navigate = useNavigate();
+  const [status, setStatus] = useState('idle');
 
   useEffect(() => {
-    getMoviesDetails(movieID).then(setDetails);
-  }, [movieID]);
+    if (movieID === null) {
+      return;
+    }
+    // .then(setDetails);
+    async function fetchMovies() {
+      try {
+        setStatus('pending');
+        const movies = await getMoviesDetails(movieID);
+        if (movies.length === 0) {
+          return Promise.reject(
+            new Error(`Sorry, we didn't infortmaion`)
+          ).catch(error => {
+            setStatus('rejected');
+          });
+        } else {
+          setStatus('resolved');
+          setDetails(movies);
+        }
+      } catch {
+        setStatus('idle');
+      }
+    }
+    fetchMovies();
+  }, [movieID, status]);
 
   return (
     <>
-      {details ? (
+      {status === 'pending' && <LoadingComponent />}
+      {status === 'idle' ? <p>{`Hello! No information :(`}</p> : ''}
+      {status === 'resolved' ? (
+        <div>
+          <div>
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${details.poster_path}`}
+              alt={details.title}
+              width="300"
+              height="450"
+            />
+            <h1>{details.title}</h1>
+            <p>User Score: {details.vote_average}%</p>
+            <h2>Overview</h2>
+            <p>{details.overview}</p>
+            <h2>Genres</h2>
+            <p>{details.genres.map(genre => genre.name)}</p>
+          </div>
+          <div>
+            <h2>Additional Information</h2>
+            <ul>
+              <button
+                type="button"
+                onClick={() => {
+                  navigate(`cast`, { state: { from: location.state?.from } });
+                }}
+              >
+                Cast
+              </button>
+              <button
+                type="button"
+                onClick={() => {
+                  navigate(`reviews`, {
+                    state: { from: location.state?.from },
+                  });
+                }}
+              >
+                Reviews
+              </button>
+            </ul>
+          </div>
+        </div>
+      ) : (
+        ''
+      )}
+
+      {/* {details ? (
         <div>
           <div>
             <img
@@ -58,7 +127,7 @@ export const MoviesDetails = () => {
         </div>
       ) : (
         <LoadingComponent />
-      )}
+      )} */}
     </>
   );
 };
